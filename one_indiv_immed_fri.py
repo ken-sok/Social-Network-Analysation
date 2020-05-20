4788ebe382d54db1cc83fc9e9a0e2db4c7c5c825
kheang <kheang.learning@gmail.com>
2020-05-20 12:20:33 +0700

    initial commit

--- one_indiv_immed_fri.py
+++ one_indiv_immed_fri.py
@@ -1,10 +1,10 @@
-def friend_besties(individual, friend_list):
-    '''this function takes name of an individual and friend_list from immed_fri.py, 
+def friend_besties(individual, friend_dict):
+    '''this function takes name of an individual and friend_dict from immed_fri.py, 
     then find and output sorted list of degree-one friends of the specified individual
     ''' 
     
-    if individual in friend_list: 
-        return sorted(friend_list[individual])
+    if individual in friend_dict: 
+        return sorted(friend_dict[individual])
     else: 
         return []
     
